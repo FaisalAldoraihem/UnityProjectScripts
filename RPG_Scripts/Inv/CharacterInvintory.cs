df4ed9e581c05e13b27fc8fd8e874f88d0c01ece
Faisal <faisalaldoraihem@gmail.com>
2019-05-06 03:18:32 +0300

    Add files via upload
    Random Scripts that i made making a Test RPG Game

---
+++ RPG_Scripts/Inv/CharacterInvintory.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CharacterInvintory : SingleTon<CharacterInvintory>
+{
+
+    public CharacterStats charStats;
+
+    public Image[] hotbarDisplay = new Image[4];
+    public GameObject inventoryDisplay;
+    public Image[] inventoryDisplaySlots = new Image[30];
+
+    int inventoryItemCap = 20;
+    int idCount = 1;
+    bool addedItem = true;
+
+    public Dictionary<int, InventoryEntry> itemsInventory = new Dictionary<int, InventoryEntry>();
+    public InventoryEntry itemEntry;
+
+
+    private void Start()
+    {
+        itemEntry = new InventoryEntry(0, null, null);
+        itemsInventory.Clear();
+
+        inventoryDisplaySlots = inventoryDisplay.GetComponentsInChildren<Image>();
+
+        charStats = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterStats>();
+    }
+
+    public void StoreItem(ItemPickUp ItemToStore)
+    {
+        addedItem = false;
+
+        if((charStats.characterDefinition.currentEncumbrance + ItemToStore.itemDefinition.itemWeight) <= charStats.characterDefinition.maxEncumbrance)
+        {
+            itemEntry.invEntry = ItemToStore;
+            itemEntry.stackSize = 1;
+            itemEntry.hbSprite = ItemToStore.itemDefinition.itemIcon;
+
+            ItemToStore.gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ RPG_Scripts/Inv/SingleTon.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Classic instance bois
+ * p static I 
+ * Getter 
+ * Method to check if initialized
+ * Make sure thier aint double of it in awake
+ * and a destroy
+ * */
+
+public class SingleTon<T> : MonoBehaviour where T : SingleTon<T> 
+{
+
+    private static T instance;
+
+	public static T Instance
+    {
+        get { return instance; }
+    }
+
+    public static bool Isinitialized
+    {
+        get { return instance != null; }
+    }
+
+    protected virtual void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("[SingleTon] Trying to instantiate a second instance of S class");
+        }
+        else
+        {
+            instance = (T) this;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if(instance == this)
+        {
+            instance = null;
+        }
+
+
+    }
+
+
+}
